aef2414ebfd389a367e60fe8b2a77e980677db26
QingXox <2652518585@qq.com>
2021-10-01 23:23:23 +0800

    xx

--- test.cpp
+++ test.cpp
@@ -1,9 +1,23 @@
-#include<vector> 
-#include<string> 
-#include<fstream> 
-#include<iostream>
+#include <vector> 
+#include <string> 
+#include <fstream> 
+#include <iostream>
 using namespace std;
 
 int main(){
-	cout << "yyy" << endl;
+	ifstream in("E:\\QQ Download\\yq_in.txt");
+	ofstream out("E:\\QQ Download\\yq_out.txt");
+	string s, las;
+	while(in >> s){
+		if(s != las){
+			if(las.size()) out << endl;
+			las = s;
+			out << s << endl;
+		}
+		getline(in, s);
+		s = s.substr(1);
+		if(s=="待明确地区	0") continue;
+		out << s << endl;
+	}
+	in.close(); out.close();
 }
